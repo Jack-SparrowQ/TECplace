8c8e66838a9a891ec23de17a0e245f8744a25e3f
Jack S <carlosriver041105@gmail.com>
2025-05-02 14:04:00 -0600

    implementing home login

---
+++ Home/home-module/Main-project/FeedForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Main_project
+{
+    partial class FeedForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSettings = new Button();
+            btnProfile = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnSettings
+            // 
+            btnSettings.Location = new Point(39, 51);
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(117, 40);
+            btnSettings.TabIndex = 0;
+            btnSettings.Text = "Settings";
+            btnSettings.UseVisualStyleBackColor = true;
+            btnSettings.Click += btnSettings_Click;
+            // 
+            // btnProfile
+            // 
+            btnProfile.Location = new Point(39, 3);
+            btnProfile.Name = "btnProfile";
+            btnProfile.Size = new Size(138, 42);
+            btnProfile.TabIndex = 1;
+            btnProfile.Text = "Profile";
+            btnProfile.UseVisualStyleBackColor = true;
+            btnProfile.Click += btnProfile_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Image.FromFile("C:\\Users\\Carlo\\Downloads\\imgprofile.jpeg");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(30, 35);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Image.FromFile("C:\\Users\\Carlo\\Downloads\\settings-icon.png");
+            pictureBox2.Location = new Point(3, 51);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(24, 40);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(tableLayoutPanel1);
+            flowLayoutPanel1.Dock = DockStyle.Right;
+            flowLayoutPanel1.Location = new Point(617, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(183, 450);
+            flowLayoutPanel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.Controls.Add(pictureBox2, 0, 1);
+            tableLayoutPanel1.Controls.Add(btnProfile, 1, 0);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(btnSettings, 1, 1);
+            tableLayoutPanel1.Location = new Point(3, 3);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 45.7142868F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 54.2857132F));
+            tableLayoutPanel1.Size = new Size(180, 105);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // FeedForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(flowLayoutPanel1);
+            Name = "FeedForm";
+            Text = "Form1";
+            Load += FeedForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnProfile;
+        private Button btnSettings;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}
